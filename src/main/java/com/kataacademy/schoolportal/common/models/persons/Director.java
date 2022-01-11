96fedd018e5a26d1f5615d58aecdefd220808890
Anton <medmedmedik@gmail.com>
2022-01-11 21:06:04 +0300

    SP-5061 - сеттеры и геттеры реализованы через ломбок

--- src/main/java/com/kataacademy/schoolportal/common/models/persons/Director.java
+++ src/main/java/com/kataacademy/schoolportal/common/models/persons/Director.java
@@ -1,21 +1,19 @@
 package com.kataacademy.schoolportal.common.models.persons;
 
 
-import javax.persistence.Column;
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Director extends Person {
     @Id
-    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter @Setter
     private Long id;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 }
