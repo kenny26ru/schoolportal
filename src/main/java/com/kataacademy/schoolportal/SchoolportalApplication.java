1bba410ae3733d2b17cc3d74647ebdcf40dcdaac
kenny26ru <georgey26ru@gmail.com>
2022-01-11 12:28:45 +0300

    Initial commit

---
+++ src/main/java/com/kataacademy/schoolportal/SchoolportalApplication.java
@@ -0,0 +1,13 @@
+package com.kataacademy.schoolportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolportalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kataacademy/schoolportal/SchoolportalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kataacademy.schoolportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
