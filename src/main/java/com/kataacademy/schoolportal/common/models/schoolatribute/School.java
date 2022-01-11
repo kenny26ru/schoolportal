f989affd1c4e350fd101e580356d6f13424e1af6
Julia <juliasemina>
2022-01-11 22:34:25 +0300

    5051 - добавлены аннотации для класса @getter и @setter, удален прежний вариант getters и setters

--- src/main/java/com/kataacademy/schoolportal/common/models/schoolatribute/School.java
+++ src/main/java/com/kataacademy/schoolportal/common/models/schoolatribute/School.java
@@ -1,8 +1,13 @@
 package com.kataacademy.schoolportal.common.models.schoolatribute;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 
 @Entity
+@Getter
+@Setter
 @Table(name = "school")
 public class School {
     @Id
@@ -25,38 +30,6 @@ public class School {
     public School(String name, int capacity, String address) {
         this.name = name;
         this.capacity = capacity;
-        this.address = address;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getCapacity() {
-        return capacity;
-    }
-
-    public void setCapacity(int capacity) {
-        this.capacity = capacity;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
         this.address = address;
     }
 
