11c702904b78f59cc0a5cecf26a58c6320b157f2
dmtch-github <dmtch@yandex.ru>
2022-01-11 20:26:16 +0400

    5001 - добавить модель Teacher

---
+++ src/main/java/com/kataacademy/schoolportal/common/models/persons/Teacher.java
@@ -0,0 +1,19 @@
+package com.kataacademy.schoolportal.common.models.persons;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@Getter
+@Setter
+
+@Entity
+@Table(name="teachers")
+public class Teacher extends Person{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
+}
