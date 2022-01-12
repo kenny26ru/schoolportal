9dd8939151ec85b7020f0cf3f56341a300995a46
Anton <medmedmedik@gmail.com>
2022-01-12 20:50:26 +0300

    SP 5061 - вернул аннотацию

--- src/main/java/com/kataacademy/schoolportal/common/models/persons/Person.java
+++ src/main/java/com/kataacademy/schoolportal/common/models/persons/Person.java
@@ -7,10 +7,12 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.MappedSuperclass;
 import java.time.LocalDate;
 
 @Getter @Setter
 @AllArgsConstructor
+@MappedSuperclass
 @NoArgsConstructor
 public abstract class Person {
 
