725f35a3784b8ffd15e7b50a6e53bf9987568d45
Julia <juliasemina>
2022-01-16 15:39:53 +0300

    5053 - сделать маппер для профилей

--- src/main/java/com/kataacademy/schoolportal/common/models/enums/Profile.java
+++ src/main/java/com/kataacademy/schoolportal/common/models/enums/Profile.java
@@ -2,12 +2,28 @@ package com.kataacademy.schoolportal.common.models.enums;
 
 public enum Profile {
 
-    PHYSICAL_MATHEMATICAL,
-    CHEMICAL_BIOLOGICAL,
-    INFORMATION_TECHNOLOGICAL,
-    SOCIO_ECONOMIC,
-    HUMANITARIAN,
-    LINGUISTIC,
-    ARTISTICALLY_AESTHETIC
+    PHYSICAL_MATHEMATICAL("Физико-математический"),
+    CHEMICAL_BIOLOGICAL("Химико-биологический"),
+    INFORMATION_TECHNOLOGICAL("Информационно-технологический"),
+    SOCIO_ECONOMIC("Социально-экономический"),
+    HUMANITARIAN("Гуманитарный"),
+    LINGUISTIC("Лингвистический"),
+    ARTISTICALLY_AESTHETIC("Художественно-эстетический");
 
+    private final String value;
+
+    Profile(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return "Profile{" +
+                "name='" + value + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/kataacademy/schoolportal/common/repository/schooattribute/SchoolRepository.java
+++ src/main/java/com/kataacademy/schoolportal/common/repository/schooattribute/SchoolRepository.java
@@ -2,10 +2,7 @@ package com.kataacademy.schoolportal.common.repository.schooattribute;
 
 import com.kataacademy.schoolportal.common.models.schoolatribute.School;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-
-@Repository
 public interface SchoolRepository extends JpaRepository<School, Long> {
 
 }
