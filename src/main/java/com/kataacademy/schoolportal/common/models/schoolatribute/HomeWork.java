fa237d95b618e1454f6cb66bcfdc1a0f6d336f60
Anton <medmedmedik@gmail.com>
2022-01-18 07:15:06 +0300

    5064 - убраны ненужные импорты

--- src/main/java/com/kataacademy/schoolportal/common/models/schoolatribute/HomeWork.java
+++ src/main/java/com/kataacademy/schoolportal/common/models/schoolatribute/HomeWork.java
@@ -1,7 +1,6 @@
 package com.kataacademy.schoolportal.common.models.schoolatribute;
 
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
