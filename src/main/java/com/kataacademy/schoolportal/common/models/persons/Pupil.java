39ba9a9ca5f517ca2b780296d5ff3fd119acb1dc
Danil <korotkin321@gmail.com>
2022-01-19 14:01:44 +0300

    5014 - добавить связку Teacher-Form-Pupil

--- src/main/java/com/kataacademy/schoolportal/common/models/persons/Pupil.java
+++ src/main/java/com/kataacademy/schoolportal/common/models/persons/Pupil.java
@@ -1,11 +1,13 @@
 package com.kataacademy.schoolportal.common.models.persons;
 
+import com.kataacademy.schoolportal.common.models.schoolatribute.Form;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.List;
 
 @Getter @Setter
 @NoArgsConstructor
@@ -17,6 +19,13 @@ public class Pupil extends Person {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @ManyToMany(mappedBy = "pupils")
+    private List<Teacher> teachersList;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "form_name")
+    private Form form;
 
     public Pupil(String firstName, String secondName, String lastName, String sex, LocalDate birthday) {
         super(firstName, secondName, lastName, sex, birthday);
--- src/main/java/com/kataacademy/schoolportal/common/models/persons/Teacher.java
+++ src/main/java/com/kataacademy/schoolportal/common/models/persons/Teacher.java
@@ -1,11 +1,13 @@
 package com.kataacademy.schoolportal.common.models.persons;
 
+import com.kataacademy.schoolportal.common.models.schoolatribute.Form;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.Set;
 
 @Getter @Setter
 @NoArgsConstructor
@@ -17,6 +19,22 @@ public class Teacher extends Person {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "teacher_forms",
+            joinColumns = @JoinColumn(name = "teacher_id"),
+            inverseJoinColumns = @JoinColumn(name = "form_id")
+    )
+    private Set<Form> forms;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "teacher_pupils",
+            joinColumns = @JoinColumn(name = "teacher_id"),
+            inverseJoinColumns = @JoinColumn(name = "pupil_id")
+    )
+    private Set<Pupil> pupils;
 
     public Teacher(String firstName, String secondName, String lastName, String sex, LocalDate birthday) {
         super(firstName, secondName, lastName, sex, birthday);
