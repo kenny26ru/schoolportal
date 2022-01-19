4e7319bb1c57a642eb0784ef8db95ceeafec13f2
hagrik <greddy4000@gmail.com>
2022-01-19 12:16:12 +0300

    SP-5044 - добавить мапу для группировки класс-предметы переделал

--- src/main/java/com/kataacademy/schoolportal/common/mappers/SchoolSubjectsByClassGrade.java
+++ src/main/java/com/kataacademy/schoolportal/common/mappers/SchoolSubjectsByClassGrade.java
@@ -1,52 +1,113 @@
 package com.kataacademy.schoolportal.common.mappers;
 
 import com.kataacademy.schoolportal.common.models.enums.SchoolSubjects;
+import lombok.Getter;
+import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@Component
+@Getter
 public class SchoolSubjectsByClassGrade {
 
-    Map<SchoolSubjects, List<Integer>> schoolSubjectsMap = new HashMap<>();
-    List<Integer> classGrade = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11);
+
+    private final Map<Integer, List<SchoolSubjects>> schoolSubjectsGradationMap = new HashMap<>();
 
     {
-        schoolSubjectsMap.put(SchoolSubjects.CLEANLY_WRITING, classGrade.subList(0, 1));
-        schoolSubjectsMap.put(SchoolSubjects.READING, classGrade.subList(0, 5));
-        schoolSubjectsMap.put(SchoolSubjects.HANDY_CRAFT, classGrade.subList(0, 5));
-        schoolSubjectsMap.put(SchoolSubjects.NATURAL_HISTORY, classGrade.subList(0, 6));
-        schoolSubjectsMap.put(SchoolSubjects.MATHS, classGrade.subList(0, 7));
-        schoolSubjectsMap.put(SchoolSubjects.VOCAL, classGrade.subList(0, 8));
-        schoolSubjectsMap.put(SchoolSubjects.DRAWING, classGrade);
-        schoolSubjectsMap.put(SchoolSubjects.PHYSICAL_EDUCATION, classGrade);
-        schoolSubjectsMap.put(SchoolSubjects.RUSSIAN_LANGUAGE, classGrade);
-        schoolSubjectsMap.put(SchoolSubjects.NATIVE_LANGUAGE, classGrade.subList(1, 11));
-        schoolSubjectsMap.put(SchoolSubjects.FOREIGN_LANGUAGE, classGrade.subList(3, 11));
-        schoolSubjectsMap.put(SchoolSubjects.THEOLOGY_AND_ETHICS, classGrade.subList(3, 5));
-        schoolSubjectsMap.put(SchoolSubjects.CIVICS, classGrade.subList(4, 7));
-        schoolSubjectsMap.put(SchoolSubjects.LOCAL_HISTORY, classGrade.subList(4, 7));
-        schoolSubjectsMap.put(SchoolSubjects.HISTORY, classGrade.subList(4, 11));
-        schoolSubjectsMap.put(SchoolSubjects.LITERATURE, classGrade.subList(4, 11));
-        schoolSubjectsMap.put(SchoolSubjects.LIFE_SAFETY_FUNDAMENTALS, classGrade.subList(4, 11));
-        schoolSubjectsMap.put(SchoolSubjects.TECHNOLOGY, classGrade.subList(4, 11));
-        schoolSubjectsMap.put(SchoolSubjects.GEOGRAPHY, classGrade.subList(5, 10));
-        schoolSubjectsMap.put(SchoolSubjects.BIOLOGY, classGrade.subList(5, 11));
-        schoolSubjectsMap.put(SchoolSubjects.COMPUTER_SCIENCE, classGrade.subList(5, 11));
-        schoolSubjectsMap.put(SchoolSubjects.SOCIAL_SCIENCE, classGrade.subList(5, 11));
-        schoolSubjectsMap.put(SchoolSubjects.TECHNICAL_DRAWING, classGrade.subList(6, 8));
-        schoolSubjectsMap.put(SchoolSubjects.ALGEBRA, classGrade.subList(6, 11));
-        schoolSubjectsMap.put(SchoolSubjects.GEOMETRY, classGrade.subList(6, 11));
-        schoolSubjectsMap.put(SchoolSubjects.PHYSICS, classGrade.subList(6, 11));
-        schoolSubjectsMap.put(SchoolSubjects.CHEMISTRY, classGrade.subList(7, 11));
-        schoolSubjectsMap.put(SchoolSubjects.NATURAL_SCIENCE, classGrade.subList(9, 11));
-        schoolSubjectsMap.put(SchoolSubjects.BASIC_ECONOMICS, classGrade.subList(9, 11));
-        schoolSubjectsMap.put(SchoolSubjects.JURISPRUDENCE, classGrade.subList(9, 11));
-        schoolSubjectsMap.put(SchoolSubjects.PHILOSOPHY, classGrade.subList(9, 11));
-        schoolSubjectsMap.put(SchoolSubjects.ECOLOGY, classGrade.subList(9, 11));
-        schoolSubjectsMap.put(SchoolSubjects.ASTRONOMY, classGrade.subList(10, 11));
-        schoolSubjectsMap.put(SchoolSubjects.BASIC_MILITARY_TRAINING, classGrade.subList(10, 11));
-        schoolSubjectsMap.put(SchoolSubjects.WORLD_ART_CULTURE, classGrade);
-        schoolSubjectsMap.put(SchoolSubjects.RHETORIC, classGrade);
+        schoolSubjectsGradationMap.put(1, List.of(SchoolSubjects.CLEANLY_WRITING,
+                SchoolSubjects.READING, SchoolSubjects.HANDY_CRAFT,
+                SchoolSubjects.NATURAL_HISTORY, SchoolSubjects.MATHS,
+                SchoolSubjects.VOCAL, SchoolSubjects.DRAWING,
+                SchoolSubjects.RUSSIAN_LANGUAGE, SchoolSubjects.PHYSICAL_EDUCATION));
+        schoolSubjectsGradationMap.put(2, List.of(SchoolSubjects.READING,
+                SchoolSubjects.HANDY_CRAFT, SchoolSubjects.NATURAL_HISTORY,
+                SchoolSubjects.MATHS, SchoolSubjects.VOCAL, SchoolSubjects.DRAWING,
+                SchoolSubjects.RUSSIAN_LANGUAGE, SchoolSubjects.PHYSICAL_EDUCATION,
+                SchoolSubjects.NATIVE_LANGUAGE));
+        schoolSubjectsGradationMap.put(3, List.of(SchoolSubjects.READING,
+                SchoolSubjects.HANDY_CRAFT, SchoolSubjects.NATURAL_HISTORY,
+                SchoolSubjects.MATHS, SchoolSubjects.VOCAL, SchoolSubjects.DRAWING,
+                SchoolSubjects.RUSSIAN_LANGUAGE, SchoolSubjects.PHYSICAL_EDUCATION,
+                SchoolSubjects.NATIVE_LANGUAGE));
+        schoolSubjectsGradationMap.put(4, List.of(SchoolSubjects.READING,
+                SchoolSubjects.HANDY_CRAFT, SchoolSubjects.NATURAL_HISTORY,
+                SchoolSubjects.MATHS, SchoolSubjects.VOCAL, SchoolSubjects.DRAWING,
+                SchoolSubjects.RUSSIAN_LANGUAGE, SchoolSubjects.PHYSICAL_EDUCATION,
+                SchoolSubjects.NATIVE_LANGUAGE, SchoolSubjects.THEOLOGY_AND_ETHICS,
+                SchoolSubjects.FOREIGN_LANGUAGE));
+        schoolSubjectsGradationMap.put(5, List.of(SchoolSubjects.NATURAL_HISTORY,
+                SchoolSubjects.MATHS, SchoolSubjects.VOCAL, SchoolSubjects.DRAWING,
+                SchoolSubjects.RUSSIAN_LANGUAGE, SchoolSubjects.PHYSICAL_EDUCATION,
+                SchoolSubjects.NATIVE_LANGUAGE, SchoolSubjects.THEOLOGY_AND_ETHICS,
+                SchoolSubjects.FOREIGN_LANGUAGE, SchoolSubjects.CIVICS,
+                SchoolSubjects.LOCAL_HISTORY, SchoolSubjects.HISTORY,
+                SchoolSubjects.LITERATURE, SchoolSubjects.LIFE_SAFETY_FUNDAMENTALS,
+                SchoolSubjects.TECHNOLOGY));
+        schoolSubjectsGradationMap.put(6, List.of(SchoolSubjects.MATHS,
+                SchoolSubjects.VOCAL, SchoolSubjects.DRAWING,
+                SchoolSubjects.RUSSIAN_LANGUAGE, SchoolSubjects.PHYSICAL_EDUCATION,
+                SchoolSubjects.NATIVE_LANGUAGE, SchoolSubjects.FOREIGN_LANGUAGE,
+                SchoolSubjects.CIVICS, SchoolSubjects.LOCAL_HISTORY,
+                SchoolSubjects.HISTORY, SchoolSubjects.LITERATURE,
+                SchoolSubjects.LIFE_SAFETY_FUNDAMENTALS, SchoolSubjects.TECHNOLOGY,
+                SchoolSubjects.GEOGRAPHY, SchoolSubjects.BIOLOGY,
+                SchoolSubjects.COMPUTER_SCIENCE, SchoolSubjects.SOCIAL_SCIENCE));
+        schoolSubjectsGradationMap.put(7, List.of(SchoolSubjects.VOCAL,
+                SchoolSubjects.DRAWING, SchoolSubjects.RUSSIAN_LANGUAGE,
+                SchoolSubjects.PHYSICAL_EDUCATION, SchoolSubjects.NATIVE_LANGUAGE,
+                SchoolSubjects.FOREIGN_LANGUAGE, SchoolSubjects.CIVICS,
+                SchoolSubjects.LOCAL_HISTORY, SchoolSubjects.HISTORY,
+                SchoolSubjects.LITERATURE, SchoolSubjects.LIFE_SAFETY_FUNDAMENTALS,
+                SchoolSubjects.TECHNOLOGY, SchoolSubjects.GEOGRAPHY,
+                SchoolSubjects.BIOLOGY, SchoolSubjects.COMPUTER_SCIENCE,
+                SchoolSubjects.SOCIAL_SCIENCE, SchoolSubjects.TECHNICAL_DRAWING,
+                SchoolSubjects.ALGEBRA, SchoolSubjects.GEOMETRY,
+                SchoolSubjects.PHYSICS));
+        schoolSubjectsGradationMap.put(8, List.of(SchoolSubjects.DRAWING,
+                SchoolSubjects.RUSSIAN_LANGUAGE, SchoolSubjects.PHYSICAL_EDUCATION,
+                SchoolSubjects.NATIVE_LANGUAGE, SchoolSubjects.FOREIGN_LANGUAGE,
+                SchoolSubjects.HISTORY, SchoolSubjects.LITERATURE,
+                SchoolSubjects.LIFE_SAFETY_FUNDAMENTALS, SchoolSubjects.TECHNOLOGY,
+                SchoolSubjects.GEOGRAPHY, SchoolSubjects.BIOLOGY,
+                SchoolSubjects.COMPUTER_SCIENCE, SchoolSubjects.SOCIAL_SCIENCE,
+                SchoolSubjects.TECHNICAL_DRAWING, SchoolSubjects.ALGEBRA,
+                SchoolSubjects.GEOMETRY, SchoolSubjects.PHYSICS,
+                SchoolSubjects.CHEMISTRY));
+        schoolSubjectsGradationMap.put(9, List.of(SchoolSubjects.DRAWING,
+                SchoolSubjects.RUSSIAN_LANGUAGE, SchoolSubjects.PHYSICAL_EDUCATION,
+                SchoolSubjects.NATIVE_LANGUAGE, SchoolSubjects.FOREIGN_LANGUAGE,
+                SchoolSubjects.HISTORY, SchoolSubjects.LITERATURE,
+                SchoolSubjects.LIFE_SAFETY_FUNDAMENTALS, SchoolSubjects.TECHNOLOGY,
+                SchoolSubjects.GEOGRAPHY, SchoolSubjects.BIOLOGY,
+                SchoolSubjects.COMPUTER_SCIENCE, SchoolSubjects.SOCIAL_SCIENCE,
+                SchoolSubjects.ALGEBRA, SchoolSubjects.GEOMETRY,
+                SchoolSubjects.PHYSICS, SchoolSubjects.CHEMISTRY));
+        schoolSubjectsGradationMap.put(10, List.of(SchoolSubjects.DRAWING,
+                SchoolSubjects.RUSSIAN_LANGUAGE, SchoolSubjects.PHYSICAL_EDUCATION,
+                SchoolSubjects.NATIVE_LANGUAGE, SchoolSubjects.FOREIGN_LANGUAGE,
+                SchoolSubjects.HISTORY, SchoolSubjects.LITERATURE,
+                SchoolSubjects.LIFE_SAFETY_FUNDAMENTALS, SchoolSubjects.TECHNOLOGY,
+                SchoolSubjects.GEOGRAPHY, SchoolSubjects.BIOLOGY,
+                SchoolSubjects.COMPUTER_SCIENCE, SchoolSubjects.SOCIAL_SCIENCE,
+                SchoolSubjects.ALGEBRA, SchoolSubjects.GEOMETRY,
+                SchoolSubjects.PHYSICS, SchoolSubjects.CHEMISTRY,
+                SchoolSubjects.NATURAL_SCIENCE, SchoolSubjects.BASIC_ECONOMICS,
+                SchoolSubjects.JURISPRUDENCE, SchoolSubjects.PHILOSOPHY,
+                SchoolSubjects.ECOLOGY));
+        schoolSubjectsGradationMap.put(11, List.of(SchoolSubjects.DRAWING,
+                SchoolSubjects.RUSSIAN_LANGUAGE, SchoolSubjects.PHYSICAL_EDUCATION,
+                SchoolSubjects.NATIVE_LANGUAGE, SchoolSubjects.FOREIGN_LANGUAGE,
+                SchoolSubjects.HISTORY, SchoolSubjects.LITERATURE,
+                SchoolSubjects.LIFE_SAFETY_FUNDAMENTALS, SchoolSubjects.TECHNOLOGY,
+                SchoolSubjects.BIOLOGY, SchoolSubjects.COMPUTER_SCIENCE,
+                SchoolSubjects.SOCIAL_SCIENCE, SchoolSubjects.ALGEBRA,
+                SchoolSubjects.GEOMETRY, SchoolSubjects.PHYSICS,
+                SchoolSubjects.CHEMISTRY, SchoolSubjects.NATURAL_SCIENCE,
+                SchoolSubjects.BASIC_ECONOMICS, SchoolSubjects.JURISPRUDENCE,
+                SchoolSubjects.PHILOSOPHY, SchoolSubjects.ECOLOGY,
+                SchoolSubjects.ASTRONOMY, SchoolSubjects.BASIC_MILITARY_TRAINING,
+                SchoolSubjects.WORLD_ART_CULTURE, SchoolSubjects.RHETORIC));
     }
 }
