3207cbd2633bbe1b2510181f666801220711c568
Danil <korotkin321@gmail.com>
2022-01-19 14:01:44 +0300

    5014 - добавить связку Teacher-Form-Pupil

--- src/main/java/com/kataacademy/schoolportal/common/models/schoolatribute/Form.java
+++ src/main/java/com/kataacademy/schoolportal/common/models/schoolatribute/Form.java
@@ -3,19 +3,35 @@ package com.kataacademy.schoolportal.common.models.schoolatribute;
 import com.kataacademy.schoolportal.common.models.persons.Pupil;
 import com.kataacademy.schoolportal.common.models.persons.Teacher;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.util.List;
 
-@Getter
-@Setter
+@Getter @Setter
+@NoArgsConstructor
+
+@Entity
+@Table(name = "forms")
 public class Form {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private byte number;
+
     private String name;
+
     private String formName;
+
+    @OneToMany(mappedBy = "form", fetch = FetchType.EAGER)
     private List<Pupil> pupilList;
-    private Teacher formTeacher;
+
+    @ManyToMany(mappedBy = "forms")
+    private List<Teacher> formTeacher;
+
     private String profile;
 
 }
