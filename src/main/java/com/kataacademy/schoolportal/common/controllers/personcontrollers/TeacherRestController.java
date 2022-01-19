a48f1cdd222953ff457889c657daa67f304127bc
dmtch-github <dmtch@yandex.ru>
2022-01-19 14:42:54 +0400

    fix-5003+5034+5063 переместил контроллеры в общую папку. Приложение запускается

--- src/main/java/com/kataacademy/schoolportal/common/controllers/personcontrollers/TeacherRestController.java
+++ src/main/java/com/kataacademy/schoolportal/common/controllers/personcontrollers/TeacherRestController.java
@@ -6,7 +6,7 @@ import com.kataacademy.schoolportal.common.services.persons.TeacherService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import com.kataacademy.schoolportal.common.personcontrollers.exception.PersonNotFoundException;
+import com.kataacademy.schoolportal.common.controllers.personcontrollers.exception.PersonNotFoundException;
 
 import javax.validation.Valid;
 import java.util.List;
