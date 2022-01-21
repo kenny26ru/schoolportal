b25aec057dd8465e4f37bd70dd50087c0ce8c2e7
Artem Kirillov <sprigrance@gmail.com>
2022-01-21 15:18:09 +0300

    5022 - aborted changes from Form class

--- src/main/java/com/kataacademy/schoolportal/common/models/schoolatribute/Form.java
+++ src/main/java/com/kataacademy/schoolportal/common/models/schoolatribute/Form.java
@@ -3,25 +3,35 @@ package com.kataacademy.schoolportal.common.models.schoolatribute;
 import com.kataacademy.schoolportal.common.models.persons.Pupil;
 import com.kataacademy.schoolportal.common.models.persons.Teacher;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.FetchType;
-import javax.persistence.OneToMany;
-import java.util.ArrayList;
-import java.util.List;
+import javax.persistence.*;
+import java.util.Set;
 
-@Getter
-@Setter
+@Getter @Setter
+@NoArgsConstructor
+
+@Entity
+@Table(name = "forms")
 public class Form {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private byte number;
+
     private String name;
+
     private String formName;
 
-    @OneToMany(mappedBy = "formName")
-    private List<Pupil> pupilList;
+    @OneToMany(mappedBy = "form")
+    private Set<Pupil> pupilSet;
 
+    @OneToOne(mappedBy = "teacherForm")
     private Teacher formTeacher;
+
     private String profile;
 
 }
