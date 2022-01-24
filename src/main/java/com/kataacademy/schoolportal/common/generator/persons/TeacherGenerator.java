f0dd5ce41d41a1cabdbcd0ffb7e4129a442c30ec
Deathq <vr.savostyanov@gmail.com>
2022-01-24 03:19:51 +0300

    5035 - почти полностью переделал класс

--- src/main/java/com/kataacademy/schoolportal/common/generator/persons/TeacherGenerator.java
+++ src/main/java/com/kataacademy/schoolportal/common/generator/persons/TeacherGenerator.java
@@ -5,53 +5,84 @@ import org.springframework.core.io.ClassPathResource;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class TeacherGenerator {
 
     Random generator = new Random();
-    private static File fileMale;
-    private static File fileFemale;
-    private static File fileSurname;
-    private static File filePatronMale;
-    private static File filePatronFemale;
+    private static final Path fileMale;
+    private static final Path fileFemale;
+    private static final Path fileSurname;
+    private static final Path filePatronMale;
+    private static final Path filePatronFemale;
 
     static {
-        try {
-            fileMale = new ClassPathResource("data/MaleNames").getFile();
-            fileFemale = new ClassPathResource("data/FemaleNames").getFile();
-            fileSurname = new ClassPathResource("data/Surname").getFile();
-            filePatronMale = new ClassPathResource("data/MalePatronymic").getFile();
-            filePatronFemale = new ClassPathResource("data/FemalePatronymic").getFile();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        fileMale = Path.of("data/MaleNames");
+        fileFemale = Path.of("data/FemaleNames");
+        fileSurname = Path.of("data/Surname");
+        filePatronMale = Path.of("data/MalePatronymic");
+        filePatronFemale = Path.of("data/FemalePatronymic");
     }
 
     public Teacher teacherGenerator() {
-        if(generator.nextBoolean()) {
-            List<String> fName = Arrays.asList(fileMale.list());
-            List<String> sName = Arrays.asList(filePatronMale.list());
-            List<String> lName = Arrays.asList(fileSurname.list());
-            String firstName = fName.get(generator.nextInt(fName.size()));
-            String secondName = sName.get(generator.nextInt(sName.size()));
-            String lastName = lName.get(generator.nextInt(lName.size()));
-            String sex = "М";
+        boolean flag = generator.nextBoolean();
+        String firstName = getFirstName(flag);
+        String secondName = getSecondName(flag);
+        String lastName = getLastName() + "а";
+        String sex = flag ? "М" : "Ж";
+        LocalDate birthday = randomDate();
+        return new Teacher(firstName, secondName, lastName, sex, birthday);
+    }
+
+    private List<String> getFiles(Path path) {
+        List<String> nameList = new ArrayList<>();
+        try(Stream<String> lineStream = Files.lines(path)) {
+            nameList = lineStream.collect(Collectors.toList());
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+        return nameList;
+    }
+
+    private String getFirstName(boolean sex) {
+        return getString(sex, fileMale, fileFemale);
+    }
+
+    private String getSecondName(boolean sex) {
+        return getString(sex, filePatronMale, filePatronFemale);
+    }
+
+    private String getString(boolean sex, Path maleFile, Path femaleFile) {
+        List<String> fileList;
+        if(sex) {
+            fileList = getFiles(maleFile);
+        } else {
+            fileList = getFiles(femaleFile);
+        }
+        return fileList.get(generator.nextInt(fileList.size()));
+    }
+
+    private String getLastName() {
+        List<String> firstName = getFiles(fileSurname);
+        return firstName.get(generator.nextInt(firstName.size()));
     }
 
     private LocalDate randomDate() {
-        long startEpochDay = startDate.toEpochDay();
-        long endEpochDay = endDate.toEpochDay();
-        long randomDay = ThreadLocalRandom
-                .current()
-                .nextLong(startEpochDay, endEpochDay);
-
-        return LocalDate.ofEpochDay(randomDay);
+        LocalDate startDate = LocalDate.of(1965, 1, 1);
+        long start = startDate.toEpochDay();
+        LocalDate endDate = LocalDate.now();
+        long end = endDate.toEpochDay();
+        long randomDate = ThreadLocalRandom.current().nextLong(start, end);
+        return LocalDate.ofEpochDay(randomDate);
     }
 
 }
