e4a622591731a976d8f7676659bca53421210ecd
Leonid Izosimov <90125675+Hagrik@users.noreply.github.com>
2022-01-28 16:21:46 +0300

    Update DirectorRepository.java

--- src/main/java/com/kataacademy/schoolportal/common/repository/persons/DirectorRepository.java
+++ src/main/java/com/kataacademy/schoolportal/common/repository/persons/DirectorRepository.java
@@ -4,9 +4,6 @@ import com.kataacademy.schoolportal.common.models.persons.Director;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
-
 @Repository
 public interface DirectorRepository extends JpaRepository<Director, Long> {
 }
