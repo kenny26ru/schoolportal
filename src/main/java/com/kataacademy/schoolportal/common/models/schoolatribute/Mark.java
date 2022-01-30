0fb8cf45760acc6dbfa41f508928a86602ed2867
hagrik <greddy4000@gmail.com>
2022-01-30 20:15:19 +0300

    5047 - изменить класс mark

--- src/main/java/com/kataacademy/schoolportal/common/models/schoolatribute/Mark.java
+++ src/main/java/com/kataacademy/schoolportal/common/models/schoolatribute/Mark.java
@@ -2,33 +2,20 @@ package com.kataacademy.schoolportal.common.models.schoolatribute;
 
 import com.kataacademy.schoolportal.common.models.enums.SchoolSubjects;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import java.time.LocalDate;
+import java.util.Date;
 
 @Getter
 @Setter
-@NoArgsConstructor
 
-@Entity
-@Table(name = "marks")
 public class Mark {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-
-    /*
-     * Аннотацию @ManyToOne не стал добавлять, т.к. SchoolSubjects - это Enum.
-     * Вместо этого поставил @Enumerated(EnumType.STRING)
-     */
-    @Column(name = "school_subject")
     @Enumerated(EnumType.STRING)
-//    @ManyToOne(cascade = CascadeType.MERGE, fetch = FetchType.EAGER)
     private SchoolSubjects schoolSubjects;
 
     @Min(2)
@@ -36,4 +23,6 @@ public class Mark {
     private int mark;
 
     private LocalDate dayTime;
+
+    private Date date;
 }
