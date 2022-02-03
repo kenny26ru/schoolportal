2c64ce3e7fe119d44590b5b8ae63f3d9bcc25d34
Julia <juliasemina>
2022-02-03 18:34:28 +0300

    5058 - В классе Pupil заменила Set<SubjectMarks> на List<SubjectMarks>

--- src/main/java/com/kataacademy/schoolportal/common/models/persons/Pupil.java
+++ src/main/java/com/kataacademy/schoolportal/common/models/persons/Pupil.java
@@ -7,9 +7,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
-import java.io.Serializable;
 import java.time.LocalDate;
-import java.util.Set;
+import java.util.List;
 
 @Getter
 @Setter
@@ -17,7 +16,7 @@ import java.util.Set;
 
 @Entity
 @Table(name = "pupils")
-public class Pupil extends Person implements Serializable {
+public class Pupil extends Person  {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -37,7 +36,7 @@ public class Pupil extends Person implements Serializable {
             joinColumns = @JoinColumn(name = "pupils_id"),
             inverseJoinColumns = @JoinColumn(name = "subject_mark_id")
     )
-    private Set<SubjectMarks> subjectMarks;
+    private List<SubjectMarks> subjectMarks;
 
     public Pupil(String firstName, String secondName, String lastName, String sex, LocalDate birthday) {
         super(firstName, secondName, lastName, sex, birthday);
