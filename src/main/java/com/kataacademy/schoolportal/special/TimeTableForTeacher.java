247ae663afe0b032612af13e5cfbe55f02a4cf6c
dmtch-github <dmtch@yandex.ru>
2022-02-06 22:41:07 +0400

    5009 - реализовать метод getDayTimeTable на неделю

--- src/main/java/com/kataacademy/schoolportal/special/TimeTableForTeacher.java
+++ src/main/java/com/kataacademy/schoolportal/special/TimeTableForTeacher.java
@@ -8,5 +8,5 @@ import java.util.List;
 
 public interface TimeTableForTeacher{
     DayTimeTable getDayTimeTable(Teacher teacher, LocalDate date);
-    List<DayTimeTable> getWeekTimeTable();
+    List<DayTimeTable> getWeekTimeTable(Teacher teacher, LocalDate date);
 }
