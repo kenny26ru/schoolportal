ddf43cf85bf8b42485d79c71f2973e049724b380
Julia <juliasemina>
2022-02-08 15:28:53 +0300

    5058 - добавила NoArgsConstructor

--- src/main/java/com/kataacademy/schoolportal/secutity/request/LoginRequest.java
+++ src/main/java/com/kataacademy/schoolportal/secutity/request/LoginRequest.java
@@ -2,12 +2,14 @@ package com.kataacademy.schoolportal.secutity.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
 
 @Getter @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class LoginRequest {
     @NotBlank
     private String username;
